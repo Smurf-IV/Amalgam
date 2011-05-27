cab21999f2e41e15332de7d6dc5d8d83ee20f7ba
smurfiv_cp <SND\smurfiv_cp>
2011-05-27 19:07:20 +0000

    - Add code that has been moved from Liquesce project

---
+++ AmalgamClientTray/ClientForms/ClientConfigDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AmalgamClientTray.ClientForms
+{
+   [Serializable]
+   public class ClientConfigDetails
+   {
+      // ReSharper disable UnusedAutoPropertyAccessor.Global
+      // ReSharper disable MemberCanBePrivate.Global
+
+      public List<ClientShareDetail> SharesToRestore = new List<ClientShareDetail>();
+      // ReSharper restore MemberCanBePrivate.Global
+      // ReSharper restore UnusedAutoPropertyAccessor.Global
+   }
+}
