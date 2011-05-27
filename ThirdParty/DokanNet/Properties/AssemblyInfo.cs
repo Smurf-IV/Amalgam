29e2efb5f37bc5acd2d89b2afbf1af626fbc489e
smurfiv_cp <SND\smurfiv_cp>
2011-05-27 19:32:26 +0000

    - Completed the move over to the new codeplex location

--- ThirdParty/DokanNet/Properties/AssemblyInfo.cs
+++ ThirdParty/DokanNet/Properties/AssemblyInfo.cs
@@ -37,5 +37,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2011.5.27.285")]
-[assembly: AssemblyFileVersion("2011.5.27.285")]
+[assembly: AssemblyVersion("2011.5.27.286")]
+[assembly: AssemblyFileVersion("2011.5.27.286")]
