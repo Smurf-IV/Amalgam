812749bee929dc88af77ea11c3e8ada9f7a20a34
smurfiv_cp <SND\smurfiv_cp>
2012-02-19 12:29:18 +0000

    - Fix shared Icon usage for Phase I

--- Phase I/AmalgamClientTray/Properties/AssemblyInfo.cs
+++ Phase I/AmalgamClientTray/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("12.2.19.126")]
-[assembly: AssemblyFileVersion("12.2.19.126")]
+[assembly: AssemblyVersion("12.2.19.128")]
+[assembly: AssemblyFileVersion("12.2.19.128")]
