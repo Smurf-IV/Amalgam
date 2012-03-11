2f3100a50f2601f56fb8335044f27112421c5dad
smurfiv_cp <SND\smurfiv_cp>
2012-03-11 12:09:51 +0000

    - Revamp the GPL to have seperate sections

--- Phase II/AmalgamClientTray/Properties/AssemblyInfo.cs
+++ Phase II/AmalgamClientTray/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("12.3.11.285")]
-[assembly: AssemblyFileVersion("12.3.11.285")]
+[assembly: AssemblyVersion("12.3.11.286")]
+[assembly: AssemblyFileVersion("12.3.11.286")]
